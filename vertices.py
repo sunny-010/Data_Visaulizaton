8bc40c07f2462a30fa483363d3cd2001a815f584
sunny-010 <mollamdalfesany@gmail.com>
2026-02-27 09:40:22 +0530

    Add files via upload

---
+++ vertices.py
@@ -0,0 +1,11 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+n = int(input("Enter the numbr of vertices"))
+
+if n > 3:
+    G = nx.complete_graph(n)
+    nx.draw(G, node_color='green', node_size=1500, with_labels=True)
+    plt.show()
+else:
+    print("Minimum number of vertices should be more then 3")
